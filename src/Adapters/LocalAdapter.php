d3a9b031809492d956cef997a63d578dabdf1fd2
Zhang Di <zhangdi_me@163.com>
2018-04-10 15:33:20 +0800

    本地适配器增加基本路径配置及文件夹不存在自动创建

--- src/Adapters/LocalAdapter.php
+++ src/Adapters/LocalAdapter.php
@@ -9,10 +9,29 @@ namespace iPaya\FileSystem\Adapters;
 class LocalAdapter extends Adapter
 {
     /**
+     * @var string
+     */
+    public $path;
+    /**
+     * @var bool
+     */
+    public $autoMkdir = true;
+    /**
+     * @var int
+     */
+    public $dirMode = 0777;
+
+    public function __construct($path)
+    {
+        $this->path = rtrim($path, '/');
+    }
+
+    /**
      * @inheritDoc
      */
     function write($filename, $contents)
     {
+        $filename = $this->path . '/' . ltrim($filename, '/');
         return file_put_contents($filename, $contents) !== false;
     }
 
@@ -21,6 +40,13 @@ class LocalAdapter extends Adapter
      */
     function upload($srcFilename, $destFilename)
     {
+        $destFilename = $this->path . '/' . ltrim($destFilename, '/');
+        $dir = dirname($destFilename);
+
+        if ($this->autoMkdir && !file_exists($dir)) {
+            mkdir($dir, $this->dirMode, true);
+        }
+
         if (is_uploaded_file($srcFilename)) {
             return move_uploaded_file($srcFilename, $destFilename);
         } else {
@@ -33,6 +59,8 @@ class LocalAdapter extends Adapter
      */
     function readContents($filename)
     {
+        $filename = $this->path . '/' . ltrim($filename, '/');
+
         return file_get_contents($filename);
     }
 
